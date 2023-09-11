d905c09b1d4dfb1e81b3421290d437594303ff5f
lstubdal <lotte.stubdal@gmail.com>
2023-09-11 21:06:23 +0200

    Initial Commit

---
+++ eventlogger-natteravnene/eventlogger_natteravneneApp.swift
@@ -0,0 +1,17 @@
+//
+//  eventlogger_natteravneneApp.swift
+//  eventlogger-natteravnene
+//
+//  Created by Lotte Stubdal on 11/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct eventlogger_natteravneneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
